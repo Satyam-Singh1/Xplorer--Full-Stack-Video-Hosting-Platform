10c76df38bc1c431360c88e7be3919d2aec67de2
Satyam Singh <satyamrajput9956@gmail.com>
2025-04-17 08:28:12 +0530

    Model Added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+  // Configuration
+  cloudinary.config({ 
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+      api_key:process.env.CLOUDINARY_API_KEY,
+      api_secret:process.env.CLOUDINARY_API_SECRET
+  });
+
+
+  const uploadOnCloudinary = async(localFilePath)=>{
+    try {
+         if(!localFilePath){return null;}
+         //if filepathavailabe upload the file to cloudinary
+    const response =  await cloudinary.uploader.upload(localFilePath,{
+        resource_type:"auto"
+      })
+
+      //If file has been uploaded successfully 
+      console.log("File is uploaded on cloudinary", response.url);
+      return response;
+
+    } catch (error) {
+       fs.unlinkSync(localFilePath) //after uploading remove the locally saved file as upload operation got failed
+    }
+  }
+
+  export {uploadOnCloudinary}
